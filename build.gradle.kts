3070d10bc6643e0131b35606c1e2059d751de733
mrAppleXZ <mr.applexz@gmail.com>
2020-11-27 03:42:27 +0500

    Create buildscripts

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+}
+
+val projectVersion: String by project
+val jsoupVersion: String by project
+val kotlinxCoroutinesVersion: String by project
+val jvmTarget: String by project
+val kotlinStdlib: String by project
+val kotlinxSerializationVersion: String by project
+val progressbarVersion: String by project
+
+group = "net.pearx.scraper.bashim"
+version = projectVersion
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib${if (kotlinStdlib.isEmpty()) "" else "-$kotlinStdlib"}"))
+    implementation("org.jsoup:jsoup:$jsoupVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
+    implementation("me.tongfei:progressbar:$progressbarVersion")
+}
+
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = jvmTarget
+    }
+}
